ed54b44a6cfa6a109248ecefc9f9907514c5e77b
Samuel Ho <samuelho@Samuels-MacBook-Pro.local>
2024-10-28 14:37:40 -0400

    updated base state

--- kernel/conv.c
+++ kernel/conv.c
@@ -37,7 +37,68 @@ float **kernel_flatten(float ****kernel, int num_kernels, int kernel_size)
 // Basic convolution operation
 float ***convolution(float ***image, int numChannels, float ****kernel, float *biasData, int numFilters, int inputSize, int kernelSize)
 {
-    // TODO: Implement the basic convolution operation
+    // DONE: Implement the convolution operation
+
+    // also has relu 
+
+    // image: a 3d-array
+
+    // kernel[3][1][2][1] is the weight at the (2,1) position for the 3rd filter's 1st channel
+    // kernel[filter][channel][row][column]
+    // results will be summed across all CHANNELS, so output will be 3-d array (FILTER, row, column)
+
+    // mem alloc
+
+    float ***convOutput = (float ***)malloc(numFilters * sizeof(float **));
+    for (int i = 0; i < numFilters; i++) {
+        convOutput[i] = (float **)malloc((inputSize - kernelSize + 1) * sizeof(float *));
+        for (int j = 0; j < (inputSize - kernelSize + 1); j++) {
+            convOutput[i][j] = (float *)malloc((inputSize - kernelSize + 1) * sizeof(float));
+        }
+    }
+
+
+    for (int filter = 0; filter < numFilters; filter++){
+        //outermost loop
+
+        // per channel
+
+        // my assumptions: image channel = inputSize * inputSize
+        // filter window = kernelSize * kernelSize
+
+        for (int outRow = 0; outRow < inputSize - kernelSize + 1; outRow++){
+            for (int outCol = 0; outCol < inputSize - kernelSize + 1; outCol++){
+
+
+                float sum = 0.0; //init the filter
+
+
+                for (int channel = 0; channel < numChannels; channel++) {
+
+                    // want to sum across all channels
+
+
+                    for (int kRow = 0; kRow < kernelSize; kRow++) {
+                        for (int kCol = 0; kCol < kernelSize; kCol++) {
+
+                            int inRow = outRow + kRow;
+                            int inCol = outCol + kCol;
+
+                            sum += image[channel][inRow][inCol] * kernel[filter][channel][kRow][kCol];
+
+                        }
+                    }
+                }
+                convOutput[filter][outRow][outCol] = relu(sum + biasData[filter]);
+            }
+        }
+
+
+    }
+
+    return convOutput;
+
+
 }
 
 // Convolution with im2col algorithm
